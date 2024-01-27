6420d199b5398d9ada186236d5a9c5db52829b97
Abdulazizjon778 <abdulazizjonazizov778@gmail.com>
2024-01-27 13:12:51 +0500

    fix-9

--- dist/assets/AboutView-rpGn6IRE.js
+++ dist/assets/AboutView-rpGn6IRE.js
@@ -1,1 +1,7 @@
-import{_ as t}from"./index-3AR2O6wK.js";const e={};function o(r,n,s,c,_,a){return null}const f=t(e,[["render",o]]);export{f as default};
+import { _ as t } from "./index-3AR2O6wK.js";
+const e = {};
+function o(r, n, s, c, _, a) {
+  return null;
+}
+const f = t(e, [["render", o]]);
+export { f as default };
